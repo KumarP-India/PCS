38c4f0c623f423ac7d8208d0a259b74ca93e1229
Uniquely-Rare <kumar.prabhas.p2@gmail.com>
2021-05-29 20:29:25 +0530

    Added Buzzer, Livefeed AND it is ready code but to run Live-Stream & Main-Camera Program ---> We Need ONE MORE thing to do MultiThreading!

--- PCS/PCS.py
+++ PCS/PCS.py
@@ -4,6 +4,48 @@ import os
 import cv2
 
 from datetime import datetime as dtm
+from gpiozero import Buzzer as Buzz
+from flask import Flask, render_template, Response
+from flask_basicauth import BasicAuth
+
+
+
+
+# App Globals (do not edit)
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'PSK'
+app.config['BASIC_AUTH_PASSWORD'] = 'password'
+app.config['BASIC_AUTH_FORCE'] = True
+
+basic_auth = BasicAuth(app)
+
+
+
+
+
+@app.route('/')
+@basic_auth.required
+def index():
+
+    return render_template('index.html')
+
+
+def gen(frm):
+    while True:
+        ret, img = cv2.imencode('.png', frm)
+        
+        if ret:
+            
+            frame = img.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(detector(cv2.VideoCapture(0).read()[1])[1]),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 
 def detector(frame):
@@ -75,8 +117,15 @@ def space_protocol():
     print("Space-Protocol Sucessfully ran :)")
 
 
+
+
+
+
+
 if __name__ == "__main__" :
 
+    app.run(host='0.0.0.0', debug=False)
+    
     cap = cv2.VideoCapture(0)
 
     model = cv2.CascadeClassifier('models/haarcascade_upperbody.xml') # The Classifier Model
@@ -89,10 +138,22 @@ if __name__ == "__main__" :
 
     days = 15
 
+    butt = buzz(27)
+
 
     try: 
 
         parent = '/home/pi/Desktop/Recorded/'
+
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+
+        time.sleep(sp)
+
+        butt.on()
+        time.sleep(sp)
+        butt.off()
         
         while os.listdir(parent) <= days:
 
@@ -109,6 +170,10 @@ if __name__ == "__main__" :
                 elif Found == True and time.time() - last_time > threshold:
 
                     last_time = time.time()
+
+                    butt.on()
+                    time.sleep(sp)
+                    butt.off()
 
                     print("Person Detected")
 
@@ -145,15 +210,51 @@ if __name__ == "__main__" :
 
     except Exception as error:
 
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+        
+        time.sleep(sp)
+
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+        
+        time.sleep(sp)
+
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+
         print("\n\nThe Program had cought an error:\n{}\n:(".format(error))
 
 
     finally:        
     
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+        
+        time.sleep(sp)
+        
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+        
         print("\n\nClosing the Program\n")
         
         cv2.destroyAllWindows()
         cam.release()
+
+        butt.on()
+        time.sleep(sp)
+        butt.off()
+        
+        time.sleep(sp)
+        
+        butt.on()
+        time.sleep(sp)
+        butt.off()
 
         print("Sucessfully Closed the Program.\n\
               Thanks for using the PSI Camera :)")
