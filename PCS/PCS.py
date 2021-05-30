60442460be20a119d546834679e15067940475c8
Uniquely-Rare <kumar.prabhas.p2@gmail.com>
2021-05-30 20:51:09 +0530

    Usinf VNC-Viewer in place of Flask as it only runs in RPi

--- PCS/PCS.py
+++ PCS/PCS.py
@@ -21,35 +21,137 @@
 #Importing Lib.(s)
 import time
 import os
-import threading
 import cv2 # Open-CV
 
 from datetime import datetime as dtm
 from gpiozero import Buzzer as Buzz # Buzzer
 
-# Flask
-from flask import Flask, render_template, Response
-from flask_basicauth import BasicAuth
-
-from func.py import * # The Function Module
 
 
 
+# The Classification Function
+def detector(frame):
+        
+    g_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Converting the frame to Greyscale
 
-# App Globals
-app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = 'PSK' # Web - Username
-app.config['BASIC_AUTH_PASSWORD'] = 'password' # Web - Password
-app.config['BASIC_AUTH_FORCE'] = True
+    detection = model.detectMultiScale(g_frame, 1.3, 5) # Running the Classifier with g_frame as input (see its assignment above)
 
-basic_auth = BasicAuth(app)
+        
+    try:
+
+        found = detection.shape[0] # Finding the no. of detected objects, if any
+
+    else:
+
+        found = 0 # If it detected none
+
+    
+    # Putting the Rectangle aroung detected object(s)
+    for (x, y, w, h) in detection: # The dimensions
+        
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2) # The Recangle putting in frame   
+
+
+    # The boolean var. to set to true if it detected the object otherwise false
+    if found > 0:
+        obj = True 
+     
+
+    elif found == 0:
+        obj = False
+
+
+    return obj, frame # Returning boolean var of detected object and Edited frame
 
 
 
 
-# Main-Function
-def main(cap, model, Found, last_time, threshold, days, butt):
+# The Folder-Path Protocol
+def file_check():
 
+    exist = False # Boolean Var. for existing folder of todays' date
+            
+    parent = '/home/pi/Desktop/Recorded/' # The path!
+
+    dates_f = os.listdit(parent) # Listing all of the folders in Parent path
+            
+    today = str(dtm.now().day) + ' of ' + str(dtm.now().month) + 'th' # The todays' Folder name
+
+    folder = os.path.join(parent, today) # # The todays' Folder path
+
+
+
+    # Checing if todays' folder exists
+    for i in dates_f:
+
+        # If it founds today's name folder it returns exists var to true
+        if i == today:
+            exist = True
+
+
+
+    # If above doesnot founf it then it create it
+    if exist == False:
+
+        os.mkdir(folder)
+
+
+
+    return folder # returning the path to todays' folder
+
+
+
+
+# The recording Protocol
+def recorder(obj, frame):
+
+    obj.write(frame) # Writing the Frame to Videofile
+
+
+
+
+# The Space - Cleaning protocol
+def space_protocol():
+
+    parent = '/home/pi/Desktop/Recorded/' # The Parent path
+
+    folders = os.listdir(parent) # Listing the existing Folders
+
+
+    
+    # Loopinf over all of the Folders in Parent path
+    for i in folders:
+    
+        shutil.rmtree(i) # Removing the Non-Empty Folders from Path
+
+
+
+    print("Space-Protocol Sucessfully ran :)") # Print statement
+
+
+
+
+
+
+# Check if it ran as independent program
+if __name__ == "__main__" :
+    
+    cap = cv2.VideoCapture(0) # Defining and opening Camera object
+
+    model = cv2.CascadeClassifier('models/haarcascade_upperbody.xml') # The Classifier Model
+
+    # Var.(s)
+    Found = False
+
+    last_time = 0
+
+    threshold = 10 # The Sec.(s) of Recorded Videos
+
+    days = 15 # No. of days to keep
+
+    butt = buzz(27) # Buzzer
+
+  
     try: 
 
         parent = '/home/pi/Desktop/Recorded/' # The Path
@@ -169,72 +271,7 @@ def main(cap, model, Found, last_time, threshold, days, butt):
 
 
 
-# Web-App Defings
 
-@app.route('/')
-@basic_auth.required
-def index():
-
-    return render_template('index.html')
-
-
-
-def gen(frm):
-    while True:
-        ret, img = cv2.imencode('.png', frm)
-        
-        if ret:
-            
-            frame = img.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n\r\n')
-
-
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(detector(cv2.VideoCapture(0).read()[1])[1]),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-
-
-
-# Check if it ran as independent program
-if __name__ == "__main__" :
-    
-    cap = cv2.VideoCapture(0) # Defining and opening Camera object
-
-    model = cv2.CascadeClassifier('models/haarcascade_upperbody.xml') # The Classifier Model
-
-    # Var.(s)
-    Found = False
-
-    last_time = 0
-
-    threshold = 10 # The Sec.(s) of Recorded Videos
-
-    days = 15 # No. of days to keep
-
-    butt = buzz(27) # Buzzer
-
-
-
-    try:
-
-        # Running Main-Function on saperate Thread
-        t = threading.Thread(target=main, args=(cap, model, Found, last_time, threshold, days, butt))
-        t.daemon = True
-        t.start()
-
-
-        # Running the Web-App on this Thread
-        app.run(host='0.0.0.0', debug=False)
-
-
-
-    # Closing Program Protocol
     finally:        
     
         
